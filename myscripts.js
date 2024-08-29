c8c3d81b8fb1d8299c3d4f4f40426a73c1cbe081
Jake Bellm <[jbsnake13@gmail.com]>
2024-08-29 13:57:15 -0500

    Add event listener for two through zero

--- myscripts.js
+++ myscripts.js
@@ -14,13 +14,28 @@ const twoButton = document.querySelector('.two');
 twoButton.addEventListener('click', () => display.value = displayValue += '2');
 
 const threeButton = document.querySelector('.three');
+threeButton.addEventListener('click', () => display.value = displayValue += '3');
+
 const fourButton = document.querySelector('.four');
+fourButton.addEventListener('click', () => display.value = displayValue += '4');
+
 const fiveButton = document.querySelector('.five');
+fiveButton.addEventListener('click', () => display.value = displayValue += '5');
+
 const sixButton = document.querySelector('.six');
+sixButton.addEventListener('click', () => display.value = displayValue += '6');
+
 const sevenButton = document.querySelector('.seven');
+sevenButton.addEventListener('click', () => display.value = displayValue += '7');
+
 const eightButton = document.querySelector('.eight');
+eightButton.addEventListener('click', () => display.value = displayValue += '8');
+
 const nineButton = document.querySelector('.nine');
+nineButton.addEventListener('click', () => display.value = displayValue += '9');
+
 const zeroButton = document.querySelector('.zero');
+zeroButton.addEventListener('click', () => display.value = displayValue += '0');
 
 const clearButton = document.querySelector('.clear');
 clearButton.addEventListener('click', () => {
@@ -35,7 +50,10 @@ const divideButton = document.querySelector('.divide');
 const multiplyButton = document.querySelector('.multiply');
 const minusButton = document.querySelector('.minus');
 const plusButton = document.querySelector('.plus');
+
 const decimalButton = document.querySelector('.decimal');
+decimalButton.addEventListener('click', () => display.value = displayValue += '.');
+
 const equalButton = document.querySelector('.equal');
 
 
